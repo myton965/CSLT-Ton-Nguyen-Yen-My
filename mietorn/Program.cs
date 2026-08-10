6bea7f24e200798f15bc36a3cd5ca6418df854a7
UEH <UEH@B507PC18>
2026-08-10 16:46:41 +0700

    Add project files.

---
+++ mietorn/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("tôi là vợ của Minseok");
+        Console.WriteLine("hãy cho tôi 10 tỷ");
+        Console.WriteLine("bonjour je m'appelle My");
+        Console.WriteLine("J'ai dix-huit ans et je suis lycenne de lycee Le Khiet");
+        Console.WriteLine("un million trois cents quatre-vingt-et-un milles dongs");
+
+   
+
+    }
+}
