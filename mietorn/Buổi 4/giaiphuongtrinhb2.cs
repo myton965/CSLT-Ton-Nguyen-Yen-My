ab2756bca595726359f2f8850ba004841435d466
minseokie wife <myton965@gmail.com>
2026-08-24 23:53:08 +0700

    update

--- mietorn/Buổi 4/giaiphuongtrinhb2.cs
+++ mietorn/Buổi 4/giaiphuongtrinhb2.cs
@@ -5,16 +5,26 @@ using System.Text;
 namespace mietorn.Buổi_4
 {
     internal class giaiphuongtrinhb2
-    {  
-        static void giaiPT(int a, int b, int c)
+    {
+        public static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             //Bài tập giải phương trình bậc 2 ax^2+bx+c=0
-            if (a==0)
+            Console.Write("Nhập hệ số a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Nhập hệ số b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Nhập hệ số c: ");
+            double c = double.Parse(Console.ReadLine());
+            giaipt(a, b, c);
+        }
+        static void giaipt(double a, double b, double c)
+        {
+            if (a == 0)
             {
-                if (b==0)
+                if (b == 0)
                 {
-                    if (c==0)
+                    if (c == 0)
                     {
                         Console.WriteLine("Vô số nghiệm, x tùy ý");
                     }
@@ -25,39 +35,39 @@ namespace mietorn.Buổi_4
                 }
                 else
                 {
-                    if (c==0)
+                    if (c == 0)
                     {
                         Console.WriteLine("x=0");
                     }
                     else
                     {
-                        Console.WriteLine($"x= {-c/b} ");
+                        Console.WriteLine($"x= {(-c / b):F2} ");
                     }
                 }
+                return;
             }
             else
             {
-                if (b==0)
+                double delta = b * b - 4 * a * c;
+                if (delta < 0)
                 {
-                    if (c==0)
-                    {
-                        Console.WriteLine("x=0");
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                    }
+                    Console.WriteLine("Phương trình vô nghiệm thực");
+                }
+                else if (delta == 0)
+                {
+                    double x = -b / (2 * a);
+                    Console.WriteLine($"Phương trình có nghiệm kép: x1=x2= {x:F2}");
                 }
                 else
                 {
-
+                    double sqrtDelta = Math.Sqrt(delta);
+                    double x1 = (-b + sqrtDelta) / (2 * a);
+                    double x2 = (-b - sqrtDelta) / (2 * a);
+                    Console.WriteLine($"Phương trình có 2 nghiệm phân biệt: x1= {x1:F2} và x2={x2:F2}");
                 }
+
             }
-
         }
-               
-            
-
-        
     }
 }
+
